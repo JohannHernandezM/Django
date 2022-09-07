285de404e949c69edebe1fbf2a6e7d6b91bcdd21
unknown <johann.hernandezm@gmail.com>
2022-09-07 12:15:24 -0500

    Segundo commit con Django

--- holamundo/miembros/urls.py
+++ holamundo/miembros/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new', views.new, name='newMember'),
+    path('read', views.read, name='getMembers')
+       
+    #path('ruta1', views.ruta1, name='ruta1'),
+    #path('ruta2', views.ruta2, name='ruta2'),
+    #path('pacientes', views.datosPacientes, name='datosPacientes')
+
 ]
--- holamundo/miembros/views.py
+++ holamundo/miembros/views.py
@@ -1,5 +1,65 @@
+import json
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest
+from .models import Member
 
 def index(request):
     return HttpResponse("Hola, mundo")
+
+def new(request):
+    print("Method: ", request.method)
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+    #    print(data)
+    #    print(type(data))
+            member = Member(
+                id = data['doc'],
+                name = data['nombre'],
+                email = data['email']
+            )
+            member.save()
+            return HttpResponse("Está registrando datos.")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados.")
+    else:
+        return HttpResponseNotAllowed(["POST"], "Método inválido.")
+
+def read(request):
+    if request.method == 'GET':
+        members = Member.objects.all()
+        allMembersData = []
+        #print(members)
+        for x in members:
+            memberData = {"documento": x.id, "nombre": x.name, "correo": x.email}
+            allMembersData.append(memberData)
+        #print(json.dumps(allMembersData))
+
+        #    print(x.id)
+        #    print(x.name)
+        #    print(x.email)
+        resp = HttpResponse()
+        resp.headers['Content-Type'] = 'text/json'
+        resp.content = json.dumps(allMembersData)
+        return resp           
+    #    return HttpResponse(allMembersData)
+    #    return HttpResponse("Está consultando datos.")
+    else:
+        return HttpResponseNotAllowed(["GET"], "Método inválido")
+    
+
+#    print(request)
+#    print(dir(request))
+#    print("Headers: ", request.headers)
+#    print("Method: ", request.method)
+#    print("Body: ", request.body)
+#    print("Path: ", request.path)
+    
+#def ruta1(request):
+#    return HttpResponse("Hola, acá ruta 1")
+
+#def ruta2(request):
+#    return HttpResponse("Hola, acá ruta 2")
+
+#def datosPacientes(request):
+#    return HttpResponse("Acá irá la información de los pacientes")
